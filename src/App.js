f94e66d560f9aaba55eb00b1dc14ff1c86473073
theCodeNilesh <61627365+theCodeNilesh@users.noreply.github.com>
2021-12-17 21:38:43 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+
+import "bootstrap/dist/css/bootstrap.min.css";
+
+import { Container } from "reactstrap";
+
+const url = "https://source.unsplash.com/random";
 
 function App() {
+  let vh = window.innerHeight * 0.01;
+  document.documentElement.style.setProperty("--vh", `${vh}px`);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container className="App md-12 module">
+      <style>{"body { background-color: #2F3042; }"}</style>
+      <img className="img" src={url} alt="img" />
+    </Container>
   );
 }
 
